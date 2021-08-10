9fea9d56eeef2e8f3e0a150b1da6926bed72d277
Todd Stout <todd.tstout@gmail.com>
2021-08-10 06:26:56 -0500

    adjust formatting

--- src/sys_loader/core.clj
+++ src/sys_loader/core.clj
@@ -1,12 +1,17 @@
 (ns sys-loader.core
   (:require [clojure.tools.cli :refer [parse-opts]]
-             [clojure.edn :as edn]
-             [clojure.string :as s]))
+            [clojure.edn :as edn]
+            [clojure.string :as s]))
 
 (defn load-plugin [url]
   (let [{:keys [description init]} (edn/read-string (slurp url))]
     (println (str "loading module: " description))
-    (-> init str (s/split #"/") first symbol require)
+    (-> init
+        str
+        (s/split #"/")
+        first
+        symbol
+        require)
     ((resolve init))))
 
 (defn load-plugins []
