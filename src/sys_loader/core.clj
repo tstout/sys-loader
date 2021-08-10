39e258cb1d6f6ef210a12fba6d24f32266954978
Todd Stout <todd.tstout@gmail.com>
2021-08-09 19:05:29 -0500

    initial experiments with loading plugins from multiple repos

--- src/sys_loader/core.clj
+++ src/sys_loader/core.clj
@@ -1,4 +1,24 @@
 (ns sys-loader.core
-  (:require [clojure.tools.cli :refer [parse-opts]]))
+  (:require [clojure.tools.cli :refer [parse-opts]]
+             [clojure.edn :as edn]
+             [clojure.string :as s]))
 
-(defn -main [& args] (println "hello world"))
+(defn load-plugin [url]
+  (let [{:keys [description init]} (edn/read-string (slurp url))]
+    (println (str "loading module: " description))
+    (-> init str (s/split #"/") first symbol require)
+    ((resolve init))))
+
+(defn load-plugins []
+  (let [plugins (.getResources (ClassLoader/getSystemClassLoader) "plugin.edn")]
+    (loop []
+      (load-plugin (.. plugins nextElement openStream))
+      (when (.hasMoreElements plugins)
+        (recur)))))
+
+(defn init []
+  (println "-----Plugin Init---------"))
+
+
+(defn -main [& args]
+  (load-plugins))
