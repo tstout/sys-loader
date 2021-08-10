ed4633a188bfdc8acbcdf73679604c497592c9f0
Todd Stout <todd.tstout@gmail.com>
2021-08-10 07:36:13 -0500

    added topological sort

---
+++ src/sys_loader/deps.clj
@@ -0,0 +1,25 @@
+(ns sys-loader.deps)
+
+(defn find-a-node [deps already-have-nodes]
+  (some (fn [[k v]] (when (empty? (remove already-have-nodes v)) k)) deps))
+
+(defn order-deps
+  "Topological sort to determine proper dependency order.
+   Based on this discussion: 
+   https://groups.google.com/g/clojure/c/-sypb2Djhio/m/r9AzRpwTgRkJ"
+  [deps]
+  (loop [deps deps already-have-nodes #{} output []]
+    (if (empty? deps)
+      output
+      (if-let [item (find-a-node deps already-have-nodes)]
+        (recur
+         (dissoc deps item)
+         (conj already-have-nodes item)
+         (conj output item))
+        (throw (Exception. "Circular dependency."))))))
+
+
+(comment
+  (order-deps {:db [] :log [:db] :service-1 [:log :service-2] :service-2 [:log]})
+  ;;
+  )
