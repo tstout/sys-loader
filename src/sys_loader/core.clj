afe4e7ef92593b2952ef3c220a33b4ae634f44d0
Todd Stout <todd.tstout@gmail.com>
2021-09-26 17:26:12 -0500

    added silly logo and startup time

--- src/sys_loader/core.clj
+++ src/sys_loader/core.clj
@@ -1,21 +1,36 @@
 (ns sys-loader.core
   (:require [clojure.tools.cli :refer [parse-opts]]
             [taoensso.timbre :as log]
-            [sys-loader.plugin :refer [load-plugins-in-order!]]))
+            [clojure.java.io :as io]
+            [sys-loader.plugin :refer [load-plugins-in-order!]])
+  (:import [java.time Instant Duration]))
 
-(defn init [_]
-  (log/info "-----Plugin Init---------"))
+(def get-time
+  (memoize (fn [_] (Instant/now))))
 
+(defn t-diff-ms [a b]
+  (-> (Duration/between (get-time a) (get-time b))
+      .toMillis))
 
 (defn -main [& args]
+  (get-time :start)
   (load-plugins-in-order!)
-  (log/infof "sys-loader started"))
+  (get-time :end)
+  (log/infof "sys-loader started in %s ms" (t-diff-ms :start :end))
+  (log/info (-> "logo.txt" io/resource slurp)))
 
 
 (comment
-  (log/infof "foo bar %s" "baz")
+  *e
+  (get-time :foo)
+  (get-time :bar)
 
-  (def s (with-out-str (time (log/infof "foo bar %s" "baz"))))
-  s
+  (ns-publics (find-ns 'sys-loader.plugin))
+
+  (t-diff-ms (get-time :foo) (get-time :bar))
+
+  (log/info (-> "logo.txt" io/resource slurp))
+
+  (-> "logo.txt" io/resource slurp)
   ;;
   )
