0f5f9a9fc5e135fd402b22274280ed1adb087dc8
Todd Stout <todd.tstout@gmail.com>
2021-11-13 11:47:20 -0600

    cleanup migrations test

--- test/sys_loader/migrations_test.clj
+++ test/sys_loader/migrations_test.clj
@@ -1,26 +1,42 @@
 (ns sys-loader.migrations-test
   (:require [expectations.clojure.test :refer [defexpect expect]]
             [sys-loader.migrations :as migrate]
-            [clojure.test :refer [run-tests]]
+            [clojure.test :refer [run-tests use-fixtures]]
             [sys-loader.db :refer [mk-datasource]]
             [next.jdbc.sql :as sql]
             [next.jdbc.datafy]
-            [next.jdbc :as jdbc]
             [clojure.datafy :as d]))
 
+(def ^:dynamic *pool* nil)
+(def ^:dynamic *con* nil)
+(def ^:dynamic *migrate-fn* nil)
+
+(defn pool-setup [work]
+  (let [pool (mk-datasource :memory)
+        migrate-fn (migrate/init {:sys/db {:data-source pool}})]
+    (try
+      (binding [*pool* pool *migrate-fn* migrate-fn]
+        (work))
+      (finally
+        (.dispose pool)))))
+
+(defn connection-setup [work]
+  (with-open [con (.getConnection *pool*)]
+    (binding [*con* con]
+      (work))))
+
+(use-fixtures :once pool-setup)
+(use-fixtures :each connection-setup)
 
 (defn example-ddl [run-ddl]
   (run-ddl "users"))
 
 (defexpect migration-fn-var-is-persisted
-  (let [ds (mk-datasource :memory)
-        migrate (migrate/init {:sys/db {:data-source ds}})]
-    (expect (fn? migrate) true)
-    (migrate #'example-ddl)
-    (with-open [conn (.getConnection ds)]
-      (expect '("sys-loader.migrations-test/example-ddl")
-              (->> (sql/query conn ["select name from sys_loader.migrations"])
-                   (map :MIGRATIONS/NAME))))))
+  (expect (fn? *migrate-fn*) true)
+  (*migrate-fn* #'example-ddl)
+  (expect '("sys-loader.migrations-test/example-ddl")
+          (->> (sql/query *con* ["select name from sys_loader.migrations"])
+               (map :MIGRATIONS/NAME))))
 
 
 (comment
@@ -28,10 +44,10 @@
   *e
   (run-tests)
 
-  
+
   (def ds (mk-datasource :memory))
 
-  (def tables 
+  (def tables
     (with-open [conn (.getConnection ds)]
       (sql/query conn ["select * from information_schema.tables"])))
 
@@ -46,6 +62,8 @@
 
   (bean db-meta)
 
+  (= [1 2] '(1 2))
+  
   (type db-meta)
   (d/datafy db-meta)
 
