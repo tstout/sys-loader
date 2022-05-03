7e9baa3f14708398dbcd939971f6a362e0bda060
Todd Stout <todd.tstout@gmail.com>
2022-05-03 07:18:01 -0500

    gen main class for uberjar support

--- src/sys_loader/core.clj
+++ src/sys_loader/core.clj
@@ -4,7 +4,8 @@
             [clojure.java.io :as io]
             [clojure.edn :as edn]
             [sys-loader.module :refer [load-modules-in-order!]])
-  (:import [java.time Instant Duration]))
+  (:import [java.time Instant Duration])
+  (:gen-class))
 
 (def version-str
   (delay (-> "sys-loader.edn"
